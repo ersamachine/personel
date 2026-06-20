d6ce343c627262138008915c483bb25faa175c6c
ersamachine <easyboxmachine@gmail.com>
2026-06-20 23:39:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE="ersa-personel-v34";
+﻿const CACHE="ersa-personel-v35";
 const ASSETS=["./","index.html","config.js","manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
